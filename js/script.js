2cb61c476bccae549bb2b5d42c41b3829c7a5398
Evertonejl <evertonejl132@gmail.com>
2023-03-25 17:09:00 -0300

    menu responsivo

---
+++ js/script.js
@@ -0,0 +1,12 @@
+function menuShow() {
+    let menuMobile = document.querySelector('.mobile-menu');
+    if(menuMobile.classList.contains('open')) {
+        menuMobile.classList.remove('open');
+        document.querySelector('.icon').src = "img/icons8-menu-arredondado-50.png";
+    } else {
+        menuMobile.classList.add('open');
+        document.querySelector('.icon').src = "img/icons8-close-button-32.png";
+        
+    }
+
+}
